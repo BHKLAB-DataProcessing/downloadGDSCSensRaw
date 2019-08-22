36a73ca0f9a964a3738b054677a68dae6ba5c393
Petr Smirnov <psmirnov2000@gmail.com>
2019-08-22 12:15:34 -0400

    fix error

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 cd /pfs/out/
 wget ftp://ftp.sanger.ac.uk/pub/project/cancerrxgene/releases/current_release/GDSC1_public_raw_data_17Jul19.csv
-wget ftp://ftp.sanger.ac.uk/pub/project/cancerrxgene/releases/current_release/GDSC1_public_raw_data_17Jul19.csv
+wget ftp://ftp.sanger.ac.uk/pub/project/cancerrxgene/releases/current_release/GDSC2_public_raw_data_17Jul19.csv
